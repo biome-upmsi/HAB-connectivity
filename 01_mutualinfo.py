a073624588a5f096245a55835ee81e4f1c01d20d
apunongbayan <apunongbayan@gmail.com>
2021-01-05 17:58:41 +0800

    added separate code for mutual info

--- 01_mutualinfo.py
+++ 01_mutualinfo.py
@@ -38,7 +38,7 @@ def calc_MI(X1,X2,nvals):
             if p_xy > 0.0 and (p_x * p_y) > 0.0:
                 #Change base if needed; paper used log2
                 mutualinfo += p_xy * math.log2(p_xy / (p_x * p_y))
-    print('Mutual Info:', mutualinfo)
+    #print('Mutual Info:', mutualinfo)
     return(mutualinfo)
 
 '''
@@ -60,4 +60,42 @@ X = df.drop(['reference','datestring','year','month','datenumber'], axis='column
 #only samples from year 1991-present
 yearsub = years>=1991
 X = X[yearsub, :]
+print(X.shape)
 
+# get unique values
+xvals = np.unique(X)
+nvals = len(xvals)
+nsites = X.shape[1]
+nr = X.shape[0]
+
+MImat = np.zeros((nsites,nsites))
+AMImat = np.zeros((nsites,nsites))
+
+for s2 in range(0,nsites):
+    for s1 in range(0,nsites):
+        if s1 != s2:
+            MImat[s2, s1] = calc_MI(X[:, s1], X[:, s2], nvals)
+            AMImat[s2, s1] = adjusted_mutual_info_score(X[:, s1], X[:, s2])
+
+# Save matrix
+np.savetxt("bans_AMI.csv", AMImat, delimiter=",")
+
+var2plot = AMImat               # or MImat
+varval = '$AMI(X,Y)$ (bits)'    # or '$MI(X,Y)$ (bits)'
+
+
+fig, ax = plt.subplots()
+plot2 = ax.imshow(var2plot,'gray_r')
+ticks = range(0,11)
+print(ticks)
+ticklabels = ['Calbayog: 0','Cambatutay: 1','Irong-Irong: 2','Maqueda: 3','Villareal: 4','Daram Island: 5','Biliran: 6',
+              'Carigara: 7','Coastal Leyte: 8','Calubian: 9','San Pedro Bay: 10']
+ax.set_xticks(ticks)
+#ax.set_xticklabels(ticklabels, rotation=70)
+ax.set_yticks(ticks)
+ax.set_yticklabels(ticklabels)
+cbar = plt.colorbar(plot2)
+cbar.set_label(varval, rotation=90)
+plt.tight_layout()
+plt.show()
+#fig.savefig("fig_AMI.pdf", bbox_inches='tight')
