f6242066c3b3c9726d80882881c4749942cc6c48
apunongbayan <apunongbayan@gmail.com>
2021-01-05 14:46:10 +0800

    clean BN struc learning code for sharing

--- 00_learnBNstructure.py
+++ 00_learnBNstructure.py
@@ -1,10 +1,8 @@
 from pomegranate import BayesianNetwork
-import numpy as np
-import math
-import seaborn, time
-seaborn.set_style('whitegrid')
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn
+seaborn.set_style('whitegrid')
 
 
 df = pd.read_csv("bans_all.csv")       # v1 - irregular grid, updated to sep 2019
@@ -18,3 +16,42 @@ X = df.drop(['reference','datestring','year','month','datenumber'], axis='column
 yearsub = years>=1991
 X = X[yearsub, :]
 #print(X.shape)
+
+smoother = 1.0      # final version uses 1.0
+model = BayesianNetwork.from_samples(X, algorithm='exact-dp', pseudocount=smoother)
+print('model summary', model.copy())
+
+'''
+# Save model so we can load at a later time
+with open("bansmodel.json", "w") as outfile: outfile.write(model.to_json())
+#read back model
+#BayesianNetwork.from_json("bansmodel.json")
+'''
+
+print(model.structure)
+plt.figure(figsize=(7, 7))
+model.plot()
+plt.show()
+
+# Example inference: Predict based on observed nodes
+# 0 - clear; 1 - ban start; 2 - ban continuation
+calbayog = None     # X_0
+cambatutay = 2      # X_1
+irong = 2           # X_2
+maqueda = None      # X_3
+villareal = None    # X_4
+daram = None        # X_5
+biliran = None      # X_6
+carigara = 2        # X_7
+coastalleyte = None # X_8
+calubian = None     # X_9
+sanpedro = None     # X_10
+
+sitenames = ['calbayog', 'cambatutay', 'irong', 'maqueda', 'villareal', 'daram', 'biliran', 'carigara', 'coastalleyte',
+                      'calubian', 'sanpedro']
+modpred = model.predict([[calbayog, cambatutay, irong, maqueda, villareal, daram, biliran, carigara, coastalleyte,
+                      calubian, sanpedro]])
+#print(modpred)
+print('Prediction:')
+for i in range(0, len(sitenames)):
+    print(sitenames[i], ': ', modpred[0][i])
